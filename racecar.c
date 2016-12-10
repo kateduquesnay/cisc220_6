6cde5a5c12ec3c6aa498cc3faebea17713567ece
14drwr <14drwr@queensu.ca>
2016-12-10 17:14:25 -0500

    Update racecar.c

--- racecar.c
+++ racecar.c
@@ -4,15 +4,19 @@
 //Adele McCallum 10093867
 //Dean Wilkins-Reeves 10176758
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
 
 int position[5] = {0, 0, 0, 0, 0};
 
 int main() {
-
-  
-  
-  
+	
 }//end main
+
+
 
 int drawingtrack() {
 	system("clear");
@@ -35,6 +39,11 @@ int drawingtrack() {
 }//end drawingtrack
 
 int checkwinner(){
-	
+	for(int i=0; i < 5; i++) { 
+		if (position[i] >= 40) {
+			return (i+1);
+		}
+	}
+	return (0);
 }//end checkwinner
 
