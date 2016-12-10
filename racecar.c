c7150571691499c07b898864e7872bc47bbf7767
14drwr <14drwr@queensu.ca>
2016-12-10 16:59:21 -0500

    Update racecar.c

--- racecar.c
+++ racecar.c
@@ -5,6 +5,7 @@
 //Dean Wilkins-Reeves 10176758
 
 
+int position[5] = {0, 0, 0, 0, 0};
 
 int main() {
 
@@ -13,19 +14,27 @@ int main() {
   
 }//end main
 
-int drawingtrack(){
-  
-  
-  system("clear");
+int drawingtrack() {
+	system("clear");
 	printf(" Welcome to CISC220 Racing Arena \n Hit Enter to move forward \n");
-  
-
-  
-  
+	
+	for(int i=0; i < 5; i++) {
+		for(int k=0; k < position[i]; k++) {
+			putchar('~');
+		}
+		printf("|->");
+		
+		int remainingpos = 40 - position[i];
+		for (int j=0; j<remainingpos; j++) {
+			putchar(' ');
+		}
+		printf("# Lane %d #\n", i+1);
+	}
+	sleep(20);
+	checkwinner();
 }//end drawingtrack
 
 int checkwinner(){
-  
-  
+	
 }//end checkwinner
 
